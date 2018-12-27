b4896e82e7ba5dcc327aaa1215096c38f35ac6da
Ivo Nikic <ivo.nikic@sciencelogic.com>
2018-12-28 01:54:21 +0200

    Added more features on Register page

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,13 +1,91 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import firebase from '../../firebase';
 import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
 
 class Register extends React.Component {
-    state = {}
+    state = {
+        username: '',
+        email: '',
+        password: '',
+        passwordConfirmation: '',
+        errors: [],   
+        loading: false,
+    };
 
-    handleChange = () => {}
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    };
 
-    render() {
+    isFormEmpty = () => {
+        const { username, email, password, passwordConfirmation } = this.state;
+        return !username.length || !email.length || !password.length || !passwordConfirmation.length;
+    }
+
+    isPasswordValid = () => {
+        const { password, passwordConfirmation } = this.state;
+        if(password.length < 6 || passwordConfirmation.length < 6) {
+            return false;
+        } else if (password !== passwordConfirmation) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    isFormValid = () => {
+        let errors = [];
+        let error;
+          
+        if(this.isFormEmpty()) {
+            error = { message: 'Fill in all fields' };
+            this.setState({ errors: errors.concat(error) });
+            return false;
+        } else if (!this.isPasswordValid()) {
+            error = { message: 'Password is invalid' };
+            this.setState({ errors: errors.concat(error) });
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        if(this.isFormValid()) {
+            this.setState({
+                errors: [],
+                loading: true,
+            });
+            firebase
+                .auth()
+                .createUserWithEmailAndPassword(this.state.email, this.state.password)
+                .then(createdUser => {
+                    this.setState({ loading: false });
+                    console.log('createdUser', createdUser);
+                })
+                .catch(err => {
+                    this.setState({ errors: this.state.errors.concat(err), loading: false });
+                    console.error(err);
+                })
+        }
+    };
+
+    displayErrors = errors => errors.map((error, i) => <p key={i}>{error.message}</p>);
+
+    handleInputError = (inputName) => {
+        return this.state.errors.some(error => 
+                error.message.toLowerCase().includes(inputName)
+                ) 
+                ? 'error' 
+                : '';
+    };
+
+    render() {  
+        const { username, email, password, passwordConfirmation, errors, loading  } = this.state;
+
         return(
             <Grid textAlign="center" verticalAlign="middle" className="register">
                 <Grid.Column style={{ maxWidth: 450 }}>
@@ -15,7 +93,7 @@ class Register extends React.Component {
                         <Icon name="puzzle piece" color="black" />
                         Register for Chat
                     </Header>
-                    <Form size="large">
+                    <Form size="large" onSubmit={this.handleSubmit}>
                         <Segment stacked>
                             <Form.Input 
                                 fluid 
@@ -24,6 +102,7 @@ class Register extends React.Component {
                                 iconPosition="left" 
                                 placeholder="Username"
                                 type="text"
+                                value={username}
                                 onChange={this.handleChange}
                             />
                             <Form.Input 
@@ -33,6 +112,8 @@ class Register extends React.Component {
                                 iconPosition="left" 
                                 placeholder="Email Address"
                                 type="email"
+                                value={email}
+                                className={this.handleInputError('email')}
                                 onChange={this.handleChange}
                             />
                             <Form.Input 
@@ -42,6 +123,8 @@ class Register extends React.Component {
                                 iconPosition="left" 
                                 placeholder="Password"
                                 type="password"
+                                value={password}
+                                className={this.handleInputError('password')}
                                 onChange={this.handleChange}
                             />
                             <Form.Input 
@@ -51,18 +134,29 @@ class Register extends React.Component {
                                 iconPosition="left" 
                                 placeholder="Password Confirmation"
                                 type="password"
+                                value={passwordConfirmation}
+                                className={this.handleInputError('password')}
                                 onChange={this.handleChange}
                             />
                             <Button 
                                 fluid
                                 color="blue"
                                 size="large"
+                                disabled={loading}
+                                className={loading ? 'loading' : ''}
                             >
                                 Submit
                             </Button>
                             <Message>Already a user?<Link to="/login"> Login</Link></Message>
                         </Segment>
                     </Form>
+                    {
+                        errors.length > 0 &&  
+                            <Message error>
+                                <h3>Error</h3>
+                                {this.displayErrors(errors)}
+                            </Message>
+                    }
                 </Grid.Column>
             </Grid>
         )
