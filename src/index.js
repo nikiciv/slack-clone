a40d064ab026cf361b603d4d04b829bc023e06d9
Ivo Nikic <ivo.nikic@sciencelogic.com>
2018-12-28 15:10:02 +0200

    Added Redux

--- src/index.js
+++ src/index.js
@@ -1,24 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import 'semantic-ui-css/semantic.min.css'
+import firebase from './firebase';
 
-const Root = () => (
-    <Router>
-        <Switch>
-            <Route exact path="/" component={App}/>
-            <Route path="/login" component={Login}/>
-            <Route path="/register" component={Register}/>
-        </Switch>
-    </Router>
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import { setUser } from './actions';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+
+class Root extends React.Component {
+
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            if(user) {
+                console.log(user);
+                this.props.setUser(user);
+                this.props.history.push('/');
+            }
+        })
+    }
+
+    render() {
+        return(
+            <Switch>
+                <Route exact path="/" component={App}/>
+                <Route path="/login" component={Login}/>
+                <Route path="/register" component={Register}/>
+            </Switch>
+        )
+    }
+};
+
+const RootWithAuth = withRouter(connect(null, { setUser })(Root));
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <RootWithAuth />
+        </Router>
+    </Provider>,
+    document.getElementById('root')
 );
-   
-
-
-ReactDOM.render(<Root />, document.getElementById('root'));
 
 
