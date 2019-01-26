ea798e2c9678e3f370a52e4d757a7b46211cf116
Ivo Nikic <ivo.nikic@sciencelogic.com>
2019-01-26 12:44:02 +0200

    Added Color Picker Modal

--- src/components/ColorPanel/index.js
+++ src/components/ColorPanel/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import { Sidebar, Menu, Divider, Button } from 'semantic-ui-react';
+import { 
+    Sidebar, 
+    Menu, 
+    Divider, 
+    Button,
+    Modal,
+    Icon,
+    Label, 
+} from 'semantic-ui-react';
+import { HuePicker } from 'react-color';
 
 class ColorPanel extends React.Component {
+    state = {
+        isModalOpen: false,
+    }
+
+    openModal = () => this.setState({ isModalOpen: true });
+
+    closeModal = () => this.setState({ isModalOpen: false });
+
     render() {
+        const { isModalOpen } = this.state;
         return(
             <Sidebar
                 as={Menu}
@@ -17,7 +35,33 @@ class ColorPanel extends React.Component {
                     icon="add"
                     size="small"
                     color="blue"
+                    onClick={this.openModal}
                 />
+                <Modal 
+                    basic
+                    open={isModalOpen}
+                    onClose={this.closeModal}
+                >
+                    <Modal.Header>Choose App Colors</Modal.Header>
+                    <Modal.Content>
+                        <Label content="Primary color"/>
+                        <HuePicker />
+                        <Label content="Secondary color"/>
+                        <HuePicker />
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <Button color="green" inverted>
+                            <Icon name="checkmark"/> Save Colors
+                        </Button>
+                        <Button 
+                            color="red" 
+                            inverted
+                            onClick={this.closeModal}
+                        >
+                            <Icon name="remove"/> Cancel
+                        </Button>
+                    </Modal.Actions>
+                </Modal>
             </Sidebar>
         )
     }
